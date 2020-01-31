f8fedebba5aec1830c84ad253f958e4d3e5aa7cf
Alex Stanciu <alex@auth0.com>
2020-01-31 00:15:22 -0500

    Cleanup

--- example/README.md
+++ example/README.md
@@ -1,3 +1,5 @@
 ## SVG Workflow Canvas
 
-This is the example project in which to load and test the react component
+This is the example project in which to load and test the react component.
+
+Viewable at https://astanciu.github.io/svg-workflow-canvas
--- example/src/App.js
+++ example/src/App.js
@@ -18,24 +18,3 @@ export default () => {
     </div>
   );
 };
-
-
-// export default class App extends Component {
-//   render() {
-//     const w1 = {
-//       nodes: [
-//         { name: 'add-new-item', id: '1', icon: 'plus-circle', position: { x: 3, y: 7 } },
-//         { name: 'create-user', id: '2', icon: 'user', position: { x: 254, y: 130 } },
-//         { name: 'upload-stuff', id: '3', icon: 'home', position: { x: -137, y: -32 } },
-//         { name: 'train-jedi', id: '4', icon: 'jedi', position: { x: 254, y: -105 } }
-//       ],
-//       connections: [{ from: '1', to: '2', id: 'h1zt2' }]
-//     };
-
-//     return (
-//       <div style={{ width: "100vw", height: "100vh" }}>
-//         <Workflow workflow={w1} scale={1} snapToGrid />
-//       </div>
-//     );
-//   }
-// }
