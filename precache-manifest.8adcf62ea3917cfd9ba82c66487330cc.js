175abfc294b4c9db8b134f23ef56fdd7c01121f1
Alex Stanciu <alex@auth0.com>
2020-01-30 23:32:17 -0500

    Updates

---
+++ precache-manifest.8adcf62ea3917cfd9ba82c66487330cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530f8e6a162b09a754af3cd77ac1e6f8",
+    "url": "/svg-workflow-canvas/index.html"
+  },
+  {
+    "revision": "6f453e8640261ea6c8e1",
+    "url": "/svg-workflow-canvas/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "4e14a7ef5a232755e343",
+    "url": "/svg-workflow-canvas/static/js/2.1a193545.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/svg-workflow-canvas/static/js/2.1a193545.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f453e8640261ea6c8e1",
+    "url": "/svg-workflow-canvas/static/js/main.c47aefd6.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/svg-workflow-canvas/static/js/main.c47aefd6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85b2144034412664f54d",
+    "url": "/svg-workflow-canvas/static/js/runtime-main.3853032e.js"
+  }
+]);
